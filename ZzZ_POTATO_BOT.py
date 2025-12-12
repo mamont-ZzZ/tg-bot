c2333763d23510e03a830f455e4e28084ba7e219
mamont-ZzZ <danik5554555@gmail.com>
2025-12-12 18:41:58 +0500

    Update ZzZ_POTATO_BOT.py

--- ZzZ_POTATO_BOT.py
+++ ZzZ_POTATO_BOT.py
@@ -9,15 +9,21 @@ bot = telebot.TeleBot('8131298081:AAGAv9Hxhyl8n5f_JZQTMxdkKcMMZaVKbBI')
 def start(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('УЧАСТВУЮ', url='https://t.me/+agJ4ymR0vTA4NWZi'))
+    markup.add(types.InlineKeyboardButton('НЕ УЧАСТВУЮ', callback_data='not_engage'))
     bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}!\nГоворит бот-помощник Даниэля.\nУ меня для тебя системное уведомление:\n\n'
                                       '🎉 <b>Событие:</b> День рождения Даниэля.\n'
                                       '📅 <b>Дата:</b> 10 января (сб)\n'
-                                      '⏰ <b>Время:</b> 16:30\n'
+                                      '⏰ <b>Время:</b> 17:00\n'
                                       '📍 <b>Место:</b> <a href="https://yandex.ru/maps/11084/orenburg-oblast/house/molodyozhnaya_ulitsa_47/YUwYdwJnSU0PQFtrfXtzcX9lbQ==/?ll=55.157089%2C51.720370&z=16.63"><ins>п. Весенний, ул. Молодежная, 47</ins></a>\n\n'
                                       '<em>Я как цифровой ассистент обязан проинформировать, что ваше присутствие значительно увеличит шансы на успешное проведение праздника.</em>\n\n'
                                       'Для подтверждения участия нажми <b>"УЧАСТВУЮ"</b>\n\n'
                                       'Ваш,\n'
-                                      'Виртуальный организатор Даниэля 🤖\n', parse_mode='html', disable_web_page_preview=True, reply_markup=markup)
+                                      'Виртуальный организатор Даниэля <a href="https://avatars.mds.yandex.net/i?id=aa558d1a89352f190eda7a4fbc738f0a_sr-5194348-images-thumbs&n=13">🤖</a>\n', parse_mode='html', disable_web_page_preview=True, reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.data == 'not_engage':
+        bot.send_message(call.message.chat.id, 'Пожалуйста свяжитесь с Даниэлем - @ABOBA_ZzZz')
 
 @bot.message_handler()
 def echo(message):
